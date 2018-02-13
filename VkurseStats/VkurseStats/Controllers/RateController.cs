ad3cfb328968e04174562cfa1ca0b1b4d64d862e
Denis Volovenko - DanDomain AS <dvo@dandomain.dk>
2018-02-13 16:31:37 +0100

    add round

--- VkurseStats/VkurseStats/Controllers/RateController.cs
+++ VkurseStats/VkurseStats/Controllers/RateController.cs
@@ -41,16 +41,16 @@ namespace VkurseStats.Controllers
                 var result = new ReturnDto {
                     Eur = new RateDto {
                         Buy = last[0].EurBuy,
-                        ChangePercentBuy = last.Count == 2 ? (last[0].EurBuy / last[1].EurBuy - 1) * 100 : 0,
+                        ChangePercentBuy = last.Count == 2 ? Math.Round((last[0].EurBuy / last[1].EurBuy - 1) * 100, 2) : 0,
                         Sell = last[0].EurSell,
-                        ChangePercentSell = last.Count == 2 ? (last[0].EurSell / last[1].EurSell - 1) * 100 : 0,
+                        ChangePercentSell = last.Count == 2 ? Math.Round((last[0].EurSell / last[1].EurSell - 1) * 100, 2) : 0,
                     },
                     Usd = new RateDto {
                         Buy = last[0].UsdBuy,
-                        ChangePercentBuy = last.Count == 2 ? (last[0].UsdBuy / last[1].UsdBuy - 1) * 100 : 0,
+                        ChangePercentBuy = last.Count == 2 ? Math.Round((last[0].UsdBuy / last[1].UsdBuy - 1) * 100, 2) : 0,
 
                         Sell = last[0].UsdSell,
-                        ChangePercentSell = last.Count == 2 ? (last[0].UsdSell / last[1].UsdSell - 1) * 100 : 0,
+                        ChangePercentSell = last.Count == 2 ? Math.Round((last[0].UsdSell / last[1].UsdSell - 1) * 100, 2) : 0,
 
                     }
                 };
